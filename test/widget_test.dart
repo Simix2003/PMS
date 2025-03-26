d974fb811d7ad2c856b17aad7701caa160f1c09a
Simix <paparo.simix@gmail.com>
2025-03-26 18:31:29 +0100

    N modifiche tutto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ix_monitor/main.dart';
+import 'package:ix_monitor/main_home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

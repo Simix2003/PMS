f51748f2665fe6ff25b5d53626a063ee70b0aa47
Simix <paparo.simix@gmail.com>
2025-04-18 14:13:54 +0200

    Fixed filters remain active on FindPage

--- lib/pages/dashboard/dashboard_data.dart
+++ lib/pages/dashboard/dashboard_data.dart
@@ -42,7 +42,16 @@ class _DashboardDataState extends State<DashboardData> {
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+
+            if (index == 1 && !autoSearch) {
+              // 🧼 Reset filters ONLY if user manually taps the tab and it's not from a bar tap
+              filtersForFindPage = null;
+            }
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.today),

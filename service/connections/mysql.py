d39700f5e510d1b8da39f1077eb81bc06e562d33
Simix <paparo.simix@gmail.com>
2025-05-16 10:22:40 +0200

    Fixed fetch issues API, removed Rive

--- service/connections/mysql.py
+++ service/connections/mysql.py
@@ -14,63 +14,6 @@ from service.routes.broadcast import broadcast_stringatrice_warning
 from service.state import global_state
 
 # ---------------- MYSQL ----------------
-DB_SCHEMA = """
-Il database contiene le seguenti tabelle:
-
-1 `objects`
-- id (PK)
-- id_modulo (VARCHAR, UNIQUE)
-- creator_station_id (FK to stations.id)
-- created_at (DATETIME)
-
-2 `stations`
-- id (PK)
-- line_id (FK to production_lines.id)
-- name (VARCHAR)
-- display_name (VARCHAR)
-- type (ENUM: 'creator', 'qc', 'rework', 'other')
-- config (JSON)
-- created_at (DATETIME)
-
-3 `production_lines`
-- id (PK)
-- name (VARCHAR)
-- display_name (VARCHAR)
-- description (TEXT)
-
-4 `productions`
-- id (PK)
-- object_id (FK to objects.id)
-- station_id (FK to stations.id)
-- start_time (DATETIME)
-- end_time (DATETIME)
-- esito (INT) -- 1 = OK, 6 = KO, 2 = In Progress ( No Esito )
-- operator_id (VARCHAR)
-- cycle_time (TIME) -- calcolato come differenza tra end_time e start_time
-- last_station_id (FK to stations.id, NULLABLE)
-
-5 `defects`
-- id (PK)
-- category (ENUM: 'Generali', 'Saldatura', 'Disallineamento', 'Mancanza Ribbon', 'I_Ribbon Leadwire', 'Macchie ECA', 'Celle Rotte', 'Lunghezza String Ribbon', 'Graffio su Cella', 'Altro')
-
-6 `object_defects`
-- id (PK)
-- production_id (FK to productions.id)
-- defect_id (FK to defects.id)
-- defect_type (VARCHAR, NULLABLE) -- usato solo per i "Generali"
-- i_ribbon (INT, NULLABLE)
-- stringa (INT, NULLABLE)
-- ribbon_lato (ENUM: 'F', 'M', 'B', NULLABLE)
-- s_ribbon (INT, NULLABLE)
-- extra_data (VARCHAR, NULLABLE)
-
-7 `station_defects`
-- station_id (FK to stations.id)
-- defect_id (FK to defects.id)
-(Chiave primaria composta: station_id + defect_id)
-
-"""""
-
 
 def get_mysql_connection():
     """
@@ -402,7 +345,7 @@ async def check_stringatrice_warnings(line_name: str, mysql_conn, settings):
 
             full_station_id = f"{station['line_name']}.{station['name']}"
 
-            # Step 4: Fetch the most recent 49 productions by end_time
+            # Step 4: Fetch the most recent 24 productions by end_time
             cursor.execute("""
                 SELECT 
                     p.id AS production_id, 
@@ -417,7 +360,7 @@ async def check_stringatrice_warnings(line_name: str, mysql_conn, settings):
                 WHERE p.last_station_id = %s AND p.id != %s
                 GROUP BY p.id
                 ORDER BY p.end_time DESC
-                LIMIT 49
+                LIMIT 24
             """, (last_station_id, prod_id))
             recent_productions = cursor.fetchall()
 

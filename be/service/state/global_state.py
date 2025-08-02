b1355976fd6b063c1e2c06498423b5bad5176456
Simix <126502063+Simix2003@users.noreply.github.com>
2025-08-02 15:06:45 +0200

    feat: async snapshot caching

--- be/service/state/global_state.py
+++ be/service/state/global_state.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, Future
 import os
 from threading import Lock, RLock
 from typing import List
@@ -92,6 +92,7 @@ xml_index = {}
 # Visual snapshot state
 visual_data: dict[str, dict] = {}
 visual_data_lock = Lock()
+visual_futures: dict[str, Future] = {}
 last_sent: dict[str, dict] = {}
 
 db_range_cache: dict[str, tuple[int, int]] = {}
